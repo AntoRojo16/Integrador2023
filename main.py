50351807fc2a48039cc2f45c405cc60a860968de
AntoRojo16 <83852226+AntoRojo16@users.noreply.github.com>
2023-05-07 13:57:25 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+from ClaseMAnejadorAlumnos import ManejadorAlumno
+from claseManejadorMateria import MateriasAprobadas
+
+if __name__=="__main__":
+    materia=MateriasAprobadas()
+    print("alumnos sin ordenar")
+    print("ordenar alumnos")
+    alumnos.mostrar()
+    print("materias")
+    print("ordenar alumnos")
+    alumnos.mostrar()
+    materia=MateriasAprobadas()
+    materia.inicializar()
+    print("materias")
+    #print(materia)
+    print('Termina el programa')
+    materia.calcularMaterias()
+    materia.ejercicio2()
+    
+
